8d2eb2984426ca7b5da62131487a977a291dbce9
Wolf7201 <volk7201@mail.ru>
2022-05-19 22:03:47 +0500

    WorkApp

---
+++ app/src/main/java/com/example/myapp2/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.myapp2
+
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import java.util.logging.Logger.global
+
+
+
+class MainActivity : AppCompatActivity() {
+    var count = 0
+    var pref: SharedPreferences? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.d("Mylog", "onCreate")
+
+        pref = getSharedPreferences("TABLE", Context.MODE_PRIVATE)
+        count = pref?.getInt("count", 1)!!
+
+        val textView = findViewById<TextView>(R.id.textView)
+        textView.text = count.toString()
+    }
+
+
+    fun onClickButton(view: View) {
+        val textView = findViewById<TextView>(R.id.textView)
+        count++
+        textView.text = count.toString()
+    }
+
+    fun saveData(res: Int) {
+        val editor = pref?.edit()
+        editor?.putInt("count", res)
+        editor?.apply()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("Mylog", "onDestroy")
+        saveData(count)
+    }
+}
